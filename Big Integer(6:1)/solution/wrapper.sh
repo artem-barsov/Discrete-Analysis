ea22bf30b7b3512a6ad3dd151315cbac78a078fd
artem-barsov <artem_barsov@mail.ru>
2021-03-30 03:18:02 +0300

    автоматизация проверки

--- Big Integer(6:1)/solution/wrapper.sh
+++ Big Integer(6:1)/solution/wrapper.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
-nTests=1000
-operation==
+nTests=100
+operation=
 
-make                                                    &&
+make -s                                                 &&
+
 echo -n "Generating tests... "                          &&
 python generator.py "$nTests" "$operation" > tests.txt  &&
 echo "OK."                                              &&
+
 echo -n "Testing python BigInteger... "                 &&
 time python BigInteger.py < tests.txt > py_out.txt      &&
 echo "OK."                                              &&
+
 echo -n "Testing my C++ BigInteger... "                 &&
 time ./solution < tests.txt > cpp_out.txt               &&
 echo "OK."                                              &&
+
 echo -n "Comparing output... "                          &&
 diff py_out.txt cpp_out.txt                             &&
 echo "OK."
-make clean
+
+echo "Start benchmark? [Y/n]"
+read ans
+case $ans in y|Y|yes|Yes|'' )
+    ./benchmark.sh ;;
+esac
+
+make -s clean
